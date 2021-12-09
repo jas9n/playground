4435f9130f9792fcbc37eb74aab983f070867c18
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2021-12-08 19:26:12 -0500

    x

--- project/js/main.js
+++ project/js/main.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("nav", window.scrollY > 0);
 });
+
+const mode = document.querySelector("#mode");
+const toggle = document.querySelector("#toggle");
+mode.addEventListener("click", function () {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    toggle.classList.add("light-waves");
+    toggle.classList.remove("dark-waves");
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    toggle.classList.add("dark-waves");
+    toggle.classList.remove("light-waves");
+  }
+});
