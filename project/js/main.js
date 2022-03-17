7e3e393cda5d92e547b00c9667490dea862c3484
jchn3805 <jasonc3805@gmail.com>
2022-03-17 10:12:28 -0400

    x

--- project/js/main.js
+++ project/js/main.js
@@ -1,1 +1,18 @@
-import "../css/style.css";
+// import "../css/style.css";
+import gsap from "gsap";
+import { CSSRulePlugin } from "gsap/CSSRulePlugin.js";
+
+gsap.registerPlugin(CSSRulePlugin);
+
+gsap.from(".anim1", { opacity: 0, duration: 1, y: -50, stagger: 0.6 });
+gsap.from("img", { opacity: 0, duration: 1, y: 30, delay: 1.4 });
+gsap.from("aside", {
+  opacity: 0,
+  duration: 1,
+  backgroundPosition: "200px, 0",
+  delay: 1.1,
+});
+gsap.to(CSSRulePlugin.getRule("span:after"), {
+  cssRule: { scaleY: 0 },
+  duration: 1,
+});
