aa369bf46afa54ee45273af516453c2032855601
jchn3805 <jasonc3805@gmail.com>
2022-03-22 09:32:24 -0400

    Update main.js

--- project/js/main.js
+++ project/js/main.js
@@ -3,16 +3,28 @@ import gsap from "gsap";
 import { CSSRulePlugin } from "gsap/CSSRulePlugin.js";
 
 gsap.registerPlugin(CSSRulePlugin);
+const rule = CSSRulePlugin.getRule("span:after");
 
-gsap.from(".anim1", { opacity: 0, duration: 1, y: -50, stagger: 0.6 });
-gsap.from("img", { opacity: 0, duration: 1, y: 30, delay: 1.4 });
-gsap.from("aside", {
-  opacity: 0,
-  duration: 1,
-  backgroundPosition: "200px, 0",
-  delay: 1.1,
-});
-gsap.to(CSSRulePlugin.getRule("span:after"), {
-  cssRule: { scaleY: 0 },
-  duration: 1,
+// gsap.from(".anim1", { opacity: 0, duration: 1, y: -50, stagger: 0.6 });
+// gsap.from("img", { opacity: 0, duration: 1, y: 30, delay: 1.4 });
+// gsap.from("aside", {
+//   opacity: 0,
+//   duration: 1,
+//   backgroundPosition: "200px, 0",
+//   delay: 1.1,
+// });
+// gsap.to(rule, {
+//   cssRule: { scaleY: 0 },
+//   duration: 1,
+// });
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.from(".anim1", { y: -50, stagger: 0.6, opacity: 0 });
+tl.to(rule, { duration: 1.8, cssRule: { scaleY: 0 } }, "-=2.2");
+tl.from("aside", { backgroundPosition: "200px 0", opacity: 0 }, "-=1.5");
+tl.from("img", { y: 50, opacity: 0 }, "-=.5");
+
+document.getElementById("cta").addEventListener("click", () => {
+  tl.reversed() ? tl.play() : tl.reverse();
 });
